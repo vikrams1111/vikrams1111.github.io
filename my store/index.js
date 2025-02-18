b6aa94273fed9ad130c232d8cea36b8f9fd1cc71
vikrams1111 <vikramsabavath1111@gmail.com>
2025-02-18 10:13:27 +0530

    p3

--- my store/index.js
+++ my store/index.js
@@ -1,97 +1,81 @@
 const products = [
-    {
-      id: 1,
-      name: "Product 1",
-      desc: "Description of the product. Description of the product. ",
-      price: 25,
-    },
-    {
-      id: 2,
-      name: "Product 2",
-      desc: "Description of the product. Description of the product. ",
-      price: 45,
-    },
-    {
-      id: 3,
-      name: "Product 3",
-      desc: "Description of the product. Description of the product. ",
-      price: 30,
-    },
+    { id: 1, name: "Product 1",desc:"Description of the product",  price: 25 },
+    { id: 2, name: "Product 2",desc:"Description of the product",  price: 45 },
+    { id: 3, name: "Product 3",desc:"Description of the product", price: 30 },
   ];
   const cart = {};
   let users = [];
-  let user = {};
+  let user = {}
+  document.write("<div id=root></div>");
   const addToCart = (id) => {
-    if (!cart[id]) cart[id] = 1;
+    if(!cart[id]) cart[id] = 1;
     showCart();
   };
-  const increment = (id) => {
-    cart[id] = cart[id] + 1;
+  const increment=(id)=>{
+    cart[id]++
+    console.log(cart)
     showCart();
-  };
-  const decrement = (id) => {
-    cart[id] = cart[id] - 1;
-    cart[id] < 1 && delete cart[id];
-    console.log(cart);
-    showCart();
-  };
-  const showTotal = () => {
-    let total = products.reduce((sum, value) => {
-      return sum + value.price * (cart[value.id] ? cart[value.id] : 0);
-    }, 0);
-  
-    divTotal.innerHTML = `Order Value: $${total}`;
-  };
-  
-  const showMain = () => {
-    let str = `
-    <div class="container">
-        <div class="header">
-          <h1>My Store</h1>
-          <h4 onclick="displayCart()">Cart:<span id="items"></span></h4>
-        </div>
-        <div class="productBlock">
-          <div id="divProducts"></div>
-        </div>
-        <div id="divCartBlock" class="cartBlock">
-          <h3>My Cart</h3>
-          <div id="divCart"></div>
-          <div id="divTotal"></div>
-          <button onclick="hideCart()">Close</button>
-        </div>
-          <hr>
-      <h4>@Copyright 2025. All rights reserved.</h4>
-      </div>
-    `;
-    root.innerHTML = str;
-    showProducts();
-  };
-  
+  }
+  const decrement=(id)=>{
+    cart[id]--
+    console.log(cart)
+    cart[id]<1 && delete cart[id]
+    showCart()
+
+  }
+  const showtotal=()=>{
+    let total=products.reduce((sum,value)=>{
+        if(cart[value.id]){
+        return sum+value.price*cart[value.id]
+        }
+        return sum
+    },0)
+    divtotal.innerHTML="Order Value:"+total
+}
   const showCart = () => {
     let str = "";
     products.map((value) => {
       if (cart[value.id]) {
         str += `
-          <li>${value.name}-$${value.price}-<button onclick='decrement(${
-          value.id
-        })'>-</button>${cart[value.id]}<button onclick='increment(${
-          value.id
-        })'>+</button>-$${value.price * cart[value.id]}</li>
-          `;
+        <li>${value.name}-$${value.price}-<button onclick=decrement(${value.id})>-</button>${cart[value.id]}<button onclick=increment(${value.id})>+</button>-${value.price*cart[value.id]}</li>
+        `;
       }
     });
     divCart.innerHTML = str;
-    let count = Object.keys(cart).length;
-    items.innerHTML = count;
-    showTotal();
+    let c=Object.keys(cart).length
+    items.innerHTML=c
+    showtotal();
   };
-  const displayCart = () => {
-    divCartBlock.style.left = "80%";
-  };
-  const hideCart = () => {
-    divCartBlock.style.left = "100%";
-  };
-  
+  const displaycart=()=>{
+    divcartblock.style.left="70%"
+  }
+  const hidecart=()=>{
+    divcartblock.style.left="100%"
+  }
+  function showMain(){
+    let str=`
+    <div class="header">
+<h1>Welcome ${user.name}</h1>
+<h4 onclick="displaycart()">Cart <span id="items"></span></h4>
+</div>
+<div class="productblock">
+<h3>Products</h3>
+<div id="divProducts"></div>
+<hr>
+</div>
+<div id="divcartblock" class="cartblock" >
+<h3>My Cart</h3>
+<div id="divCart"></div>
+<div id="divtotal"></div>
+<button onclick="hidecart()">Close</button>
+</div>
+</div>
+<hr>
+<h4>@copyright 2025.All Rights reserved</h4>
+    `
+    root.innerHTML=str
+    showProducts()
+  }
   function showLogin() {
     let str = `
     <div>
@@ -105,37 +89,6 @@ const products = [
     `;
     root.innerHTML = str;
   }
-  
-  function showForm() {
-    let str = `
-    <h2>Registration Form</h2>
-    <p><input type="text" id="name" placeholder="Name"></p>
-    <p><input type="text" id="email" placeholder="Email"></p>
-    <p><input type="password" id="password" placeholder="Password"></p>
-    <p><input type="date" id="dob"></p>
-    <p><button onclick='addUser()'>Submit</button></p>
-    <p>Already a member?<button onclick='showLogin()'>Login Here</button></p>
-    `;
-    root.innerHTML = str;
-  }
-  
-  function chkUser() {
-    let email = document.getElementById("email").value;
-    let password = document.getElementById("password").value;
-    for (let i = 0; i < users.length; i++) {
-      if (users[i].email == email && users[i].password == password) {
-        // useremail = email;
-        // username = users[i].name;
-        // currBalance = users[i].balance;
-        user = users[i];
-        showMain();
-        break;
-      } else {
-        msg.innerHTML = "Access Denied";
-      }
-    }
-  }
-  
   function addUser() {
     let name = document.getElementById("name").value;
     let email = document.getElementById("email").value;
@@ -151,18 +104,47 @@ const products = [
     users.push(user);
     showLogin();
   }
-  
+  function showForm() {
+    let str = `
+    <h2>Registration Form</h2>
+    <p><input type="text" id="name" placeholder="Name"></p>
+    <p><input type="text" id="email" placeholder="Email"></p>
+    <p><input type="password" id="password" placeholder="Password"></p>
+    <p><input type="date" id="dob"></p>
+    <p><button onclick='addUser()'>Submit</button></p>
+    <p>Already a member?<button onclick='showLogin()'>Login Here</button></p>
+    `;
+    root.innerHTML = str;
+  }
+  function chkUser() {
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+    for (let i = 0; i < users.length; i++) {
+      if (users[i].email == email && users[i].password == password) {
+        // useremail = email;
+        // username = users[i].name;
+        // currBalance = users[i].balance;
+
+        user = users[i]
+        showMain();
+        break;
+      } else {
+        msg.innerHTML = "Access Denied";
+      }
+    }
+  }
   const showProducts = () => {
     let str = "<div class='row'>";
     products.map((value) => {
       str += `
-        <div class='box'>
-        <h3>${value.name}</h3>
-        <p>${value.desc}</p>
-        <h4>$${value.price}</h4>
-        <button onclick=addToCart(${value.id})>Add to Cart</button>
-        </div>
-        `;
+      <div>
+      <div class="box">
+      <h3>${value.name}</h3>
+      <p>${value.desc}</p>
+      <h4>${value.price}</h4>
+      <button onclick=addToCart(${value.id})>Add to Cart</button></div>
+      </div>
+      `;
     });
-    divProducts.innerHTML = str + "</div>";
+    divProducts.innerHTML = str+"</div>";
   };
